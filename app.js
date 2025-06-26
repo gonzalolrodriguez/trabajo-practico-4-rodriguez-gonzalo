4a02af5f36cbb919d6ccc69c81f6fc2dba197a5d
Gonzalo <gonzalo.rodriguez677@gmail.com>
2025-06-26 14:29:46 -0300

    Se configuró app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-//------------------------------------//
-//------------- IMPORT --------------//
-//----------------------------------//
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
